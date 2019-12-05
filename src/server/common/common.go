743f1ae15b4324b009a8a49cbf3c8882d244f29e
Caleb Woodbine <calebwoodbine.public@gmail.com>
2019-12-05 17:02:47 +1300

    Add: metadata field to HTTP JSON responses

--- src/server/common/common.go
+++ src/server/common/common.go
@@ -22,36 +22,36 @@
 package common
 
 import (
-        "fmt"
-        "os"
-        "io/ioutil"
-        "net/http"
-        "log"
-        "encoding/json"
-        "flattrack.io/src/server/types"
+	"encoding/json"
+	"flattrack.io/src/server/types"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
-func GetAppPort () (output string) {
-        // determine the port for the app to run on
-        output = os.Getenv("APP_PORT")
-        if output == "" {
-                output = ":8080"
-        }
-        return output
+func GetAppPort() (output string) {
+	// determine the port for the app to run on
+	output = os.Getenv("APP_PORT")
+	if output == "" {
+		output = ":8080"
+	}
+	return output
 }
 
-func Find (slice []string, val string) (int, bool) {
-        // determine if string is in string array
-        for i, item := range slice {
-                if item == val {
-                        return i, true
-                }
-        }
-        return -1, false
+func Find(slice []string, val string) (int, bool) {
+	// determine if string is in string array
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
 }
 
-func LoadPackageJSON () (output map[string]interface{}) {
-        // return contents of package.json
+func LoadPackageJSON() (output map[string]interface{}) {
+	// return contents of package.json
 	packageJSON, err := os.Open("package.json")
 	if err != nil {
 		fmt.Println(err.Error())
@@ -62,8 +62,8 @@ func LoadPackageJSON () (output map[string]interface{}) {
 	return output
 }
 
-func JSONstoreExists () (exists bool) {
-        // determine if deployment/config.json exists / the app has been initialised
+func JSONstoreExists() (exists bool) {
+	// determine if deployment/config.json exists / the app has been initialised
 	currentWorkingDirectory, _ := os.Getwd()
 	if _, err := os.Stat(fmt.Sprintf("%v/%v/%v", currentWorkingDirectory, "deployment", "config.json")); err == nil {
 		exists = true
@@ -73,32 +73,32 @@ func JSONstoreExists () (exists bool) {
 	return exists
 }
 
-func InitJSONstore () (completed bool) {
-        // create deployment/config.json
+func InitJSONstore() (completed bool) {
+	// create deployment/config.json
 	currentWorkingDirectory, _ := os.Getwd()
 	emptyJSONstore := types.EmailStore{}
-	if ! JSONstoreExists() {
-                log.Println("Creating deployment/config.json")
+	if !JSONstoreExists() {
+		log.Println("Creating deployment/config.json")
 		err := os.MkdirAll(fmt.Sprintf("%v/%v", currentWorkingDirectory, "deployment"), os.ModePerm)
 		if err == nil {
 			completed = WriteJSONstore(emptyJSONstore)
 		} else {
-                        fmt.Println("Initialisation error:", err)
+			fmt.Println("Initialisation error:", err)
 			os.Exit(1)
 		}
 	}
 	return completed
 }
 
-func DeinitJSONstore (altConfig string) (completed bool) {
-        // remove deployment/config.json (used for tests)
-        var filePath string
-        if altConfig == "" {
-                currentWorkingDirectory, _ := os.Getwd()
-                filePath = fmt.Sprintf("%v/%v/%v", currentWorkingDirectory, "deployment", "config.json")
-        } else {
-                filePath = altConfig
-        }
+func DeinitJSONstore(altConfig string) (completed bool) {
+	// remove deployment/config.json (used for tests)
+	var filePath string
+	if altConfig == "" {
+		currentWorkingDirectory, _ := os.Getwd()
+		filePath = fmt.Sprintf("%v/%v/%v", currentWorkingDirectory, "deployment", "config.json")
+	} else {
+		filePath = altConfig
+	}
 	err := os.Remove(filePath)
 	completed = true
 	if err != nil {
@@ -107,16 +107,16 @@ func DeinitJSONstore (altConfig string) (completed bool) {
 	return completed
 }
 
-func ReadJSONstore (altConfig string) (output types.EmailStore) {
-        // read contents of deployment/config.json
-        var filePath string
-        if altConfig == "" {
-                currentWorkingDirectory, _ := os.Getwd()
-                filePath = fmt.Sprintf("%v/%v/%v", currentWorkingDirectory, "deployment", "config.json")
-        } else {
-                filePath = altConfig
-        }
-        emailStore, err := os.Open(filePath)
+func ReadJSONstore(altConfig string) (output types.EmailStore) {
+	// read contents of deployment/config.json
+	var filePath string
+	if altConfig == "" {
+		currentWorkingDirectory, _ := os.Getwd()
+		filePath = fmt.Sprintf("%v/%v/%v", currentWorkingDirectory, "deployment", "config.json")
+	} else {
+		filePath = altConfig
+	}
+	emailStore, err := os.Open(filePath)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -126,8 +126,8 @@ func ReadJSONstore (altConfig string) (output types.EmailStore) {
 	return output
 }
 
-func WriteJSONstore (content types.EmailStore) (completed bool) {
-        // write data to deployment/config.json
+func WriteJSONstore(content types.EmailStore) (completed bool) {
+	// write data to deployment/config.json
 	currentWorkingDirectory, _ := os.Getwd()
 	contentAsJSONString, _ := json.Marshal(content)
 	err := ioutil.WriteFile(fmt.Sprintf("%v/%v/%v", currentWorkingDirectory, "deployment", "config.json"), []byte(contentAsJSONString), 0644)
@@ -137,8 +137,9 @@ func WriteJSONstore (content types.EmailStore) (completed bool) {
 	return completed
 }
 
-func JSONResponse (w http.ResponseWriter, code int, output interface{}) {
-        // send a JSON response
+func JSONResponse(r *http.Request, w http.ResponseWriter, code int, output types.JSONMessageResponse) {
+	// send a JSON response
+	output.Metadata.URL = r.RequestURI
 	response, _ := json.Marshal(output)
 	//w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
@@ -147,11 +148,11 @@ func JSONResponse (w http.ResponseWriter, code int, output interface{}) {
 }
 
 func Logging(next http.Handler) http.Handler {
-        // log all requests
-        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-                // Do stuff here
-                log.Printf("%v %v %v %v %v", r.Method, r.URL, r.Proto, r.Response, r.RemoteAddr)
-                // Call the next handler, which can be another middleware in the chain, or the final handler.
-                next.ServeHTTP(w, r)
-        })
+	// log all requests
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Printf("%v %v %v %v %v %v", r.Method, r.URL, r.Proto, r.Response, r.RemoteAddr, r.Header)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
 }
--- src/server/routes/routes.go
+++ src/server/routes/routes.go
@@ -22,63 +22,65 @@
 package routes
 
 import (
+	"flattrack.io/src/server/common"
+	"flattrack.io/src/server/types"
 	"net/http"
 	"regexp"
-        "flattrack.io/src/server/types"
-        "flattrack.io/src/server/common"
 )
 
-func APIroot (w http.ResponseWriter, r *http.Request) {
-        packageJSON := common.LoadPackageJSON()
-        common.JSONResponse(w, 200, types.JSONMessageResponse{Message: "Welcome to FlatTrack.io", Version: packageJSON["version"].(string)})
+func APIroot(w http.ResponseWriter, r *http.Request) {
+	//
+	packageJSON := common.LoadPackageJSON()
+	common.JSONResponse(r, w, 200, types.JSONMessageResponse{Message: "Welcome to FlatTrack.io", Version: packageJSON["version"].(string)})
 }
 
-func APIinterested (w http.ResponseWriter, r *http.Request) {
+func APIinterested(w http.ResponseWriter, r *http.Request) {
 	var emailInForm string
 	r.ParseForm()
 	emailRegex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-        if r.Form["email"] == nil {
-                common.JSONResponse(w, 400, types.JSONMessageResponse{Message: "Email address field not found."})
-                return
-        }
+	if r.Form["email"] == nil {
+		common.JSONResponse(r, w, 400, types.JSONMessageResponse{Message: "Email address field not found."})
+		return
+	}
 	emailInForm = r.Form["email"][0]
 
-        if emailInForm == "" {
-                common.JSONResponse(w, 400, types.JSONMessageResponse{Message: "Email address field is empty."})
-                return
-        }
-        if len(emailInForm) > 70 {
-                common.JSONResponse(w, 400, types.JSONMessageResponse{Message: "Email address is longer than 70 characters."})
-                return
-        }
-
-	if matches := emailRegex.MatchString(emailInForm); !matches {
-		common.JSONResponse(w, 400, types.JSONMessageResponse{Message: "Content in email form does not match the specifications for an email."})
-                return
+	if emailInForm == "" {
+		common.JSONResponse(r, w, 400, types.JSONMessageResponse{Message: "Email address field is empty."})
+		return
+	}
+	if len(emailInForm) > 70 {
+		common.JSONResponse(r, w, 400, types.JSONMessageResponse{Message: "Email address is longer than 70 characters."})
+		return
 	}
 
-        if ! common.JSONstoreExists() {
-                common.InitJSONstore()
-        }
+	if matches := emailRegex.MatchString(emailInForm); !matches {
+		common.JSONResponse(r, w, 400, types.JSONMessageResponse{Message: "Content in email form does not match the specifications for an email."})
+		return
+	}
+
+	if !common.JSONstoreExists() {
+		common.InitJSONstore()
+	}
 
 	emailStore := common.ReadJSONstore("")
-        if _, found := common.Find(emailStore.Emails, emailInForm); found {
-		common.JSONResponse(w, 200, types.JSONMessageResponse{Message: "Added to notify list successfully."})
-                return
-        }
+	if _, found := common.Find(emailStore.Emails, emailInForm); found {
+		common.JSONResponse(r, w, 200, types.JSONMessageResponse{Message: "Added to notify list successfully."})
+		return
+	}
 
 	emailStore.Emails = append(emailStore.Emails, emailInForm)
 
 	successfullyWritten := common.WriteJSONstore(emailStore)
 	if successfullyWritten {
-		common.JSONResponse(w, 200, types.JSONMessageResponse{Message: "Added to notify list successfully."})
-                return
+		common.JSONResponse(r, w, 200, types.JSONMessageResponse{Message: "Added to notify list successfully."})
+		return
 	} else {
-		common.JSONResponse(w, 400, types.JSONMessageResponse{Message: "An error occured."})
-                return
+		common.JSONResponse(r, w, 400, types.JSONMessageResponse{Message: "An error occured."})
+		return
 	}
 }
 
-func UnknownPage (w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/#/unknown-page", 302)
+func UnknownPage(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/unknown-page", 302)
+	//common.JSONResponse(r, w, 404, types.JSONMessageResponse{Message: "This page doesn't seem to exist."})
 }
