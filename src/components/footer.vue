fb907108712164485a7df9b37fbe10e6c938e312
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-09-27 17:45:00 +1300

    Remove version

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,9 +3,9 @@
         <footer class="footer">
             <div class="content has-text-centered">
                 <p>
-                    <strong>FlatTrack</strong> and <strong>FlatTrack.io <a :href="`https://gitlab.com/flattrack/flattrack.io/-/tags/${flattrackioVersion}`" target="_blank" rel="noreferrer">({{ flattrackioVersion }})</a></strong> by <a href="https://calebwoodbine.com">Caleb Woodbine</a>.
+                  <strong><a href="https://gitlab.com/flattrack/flattrack.io" target="_blank" rel="noreferrer">FlatTrack</a></strong> and <strong><a href="https://gitlab.com/flattrack/flattrack.io" target="_blank" rel="noreferrer">FlatTrack.io</a></strong> by <a href="https://calebwoodbine.com">Caleb Woodbine</a>.
                     <br>
-                    <a href="https://gitlab.com/flattrack/flattrack.io" target="_blank" rel="noreferrer"> Source code </a> - <a href="/privacy-policy">Privacy Policy</a>
+                    <a href="https://gitlab.com/flattrack" target="_blank" rel="noreferrer"> Source code </a> - <a href="/privacy-policy">Privacy Policy</a>
                 </p>
             </div>
         </footer>
@@ -13,20 +13,11 @@
 </template>
 
 <script>
-import apiroot from '@//requests/public/apiroot'
+import apiroot from '@/requests/public/apiroot'
+import constants from '@/constants/constants'
 
 export default {
-  name: 'footer',
-  data () {
-    return {
-      flattrackioVersion: '0.0.0'
-    }
-  },
-  beforeMount () {
-    apiroot.GetAPIroot().then(resp => {
-      this.flattrackioVersion = resp.data.metadata.version
-    })
-  }
+  name: 'footer'
 }
 </script>
 
